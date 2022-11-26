5b209ed42125b4b6556ff74dadbf9bf1665668ef
sebmat003 <sebmat003@utp.edu.pl>
2022-11-26 11:52:06 +0100

    config jest

--- ngrx/src/app/app.component.spec.ts
+++ ngrx/src/app/app.component.spec.ts
@@ -1,22 +1,8 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
+import { render } from '@testing-library/angular';
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
-
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const component = render(AppComponent);
+    expect(component).toBeTruthy();
   });
 });
